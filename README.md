a236a94745d39e4fe445bae90c133974a35c79dc
Nicolas <oceanwptheme@gmail.com>
2016-12-20 00:50:08 +0100

    updated file

---
+++ README.md
@@ -0,0 +1,11 @@
+OceanWP Child Theme
+=================
+
+Child Theme for the OceanWP WordPress theme.
+
+### Usage
+Simply download the zip and upload the zip (oceanwp-child-theme-master.zip) under your WordPress dashboard at Appearance > Themes. Or extract and upload via FTP at wp-content/themes/.
+
+
+### Renaming
+You can of course rename the zip file so it isn't called oceanwp-child-theme-master.zip (you should do this so it makes more sense) and also change the "Theme Name" at the top of the style.css file.
---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Child theme functions
+ *
+ * When using a child theme (see http://codex.wordpress.org/Theme_Development
+ * and http://codex.wordpress.org/Child_Themes), you can override certain
+ * functions (those wrapped in a function_exists() call) by defining them first
+ * in your child theme's functions.php file. The child theme's functions.php
+ * file is included before the parent theme's file, so the child theme
+ * functions would be used.
+ *
+ * Text Domain: oceanwp
+ * @link http://codex.wordpress.org/Plugin_API
+ *
+ */
+
+/**
+ * Load the parent style.css file
+ *
+ * @link http://codex.wordpress.org/Child_Themes
+ */
+function oceanwp_child_enqueue_parent_style() {
+	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update your theme)
+	$theme   = wp_get_theme( 'OceanWP' );
+	$version = $theme->get( 'Version' );
+	// Load the stylesheet
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), $version );
+	
+}
+add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
