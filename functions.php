bf55e2a8f6fd736b8d6ceccb0d55fc8ea8f99431
Nicolas <oceanwptheme@gmail.com>
2017-04-23 20:57:26 +0200

    updated file

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ function oceanwp_child_enqueue_parent_style() {
 	$theme   = wp_get_theme( 'OceanWP' );
 	$version = $theme->get( 'Version' );
 	// Load the stylesheet
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ) );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
 	
 }
 add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
